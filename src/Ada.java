7aa9b8e30b5e6205ad28ddb5a3009bb093252702
rocio-corder <suarez.rocio96@gmail.com>
2019-10-11 21:05:48 -0300

    Initial commit

---
+++ src/Ada.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ada {
+
+    public static void main(String[] args) {
+        double catetoA;
+        double catetoB, hipotenusa;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ingrese cateto a de un triangulo: ");
+        catetoA = scanner.nextDouble();
+
+        System.out.println("ingrese HIPOTENUSA: ");
+        catetoB = scanner.nextDouble();
+
+
+
+
+    }
+}
